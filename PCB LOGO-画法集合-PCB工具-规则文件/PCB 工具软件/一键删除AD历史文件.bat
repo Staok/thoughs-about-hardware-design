27744f385cc8ea68e2b63878d414e7fce73c7ef4
Staok <superxhy@qq.com>
2021-10-03 17:24:48 +0800

    日常大量更新

--- PCB LOGO-画法集合-PCB工具-规则文件/PCB 工具软件/一键删除AD历史文件.bat
+++
@@ -1,7 +0,0 @@
-del *.PcbDoc.Zip/s  
-del *.SchDoc.Zip/s 
-del *.PrjPCB.Zip/s 
-del *.PcbLib.Zip/s 
-del *.SchLib.Zip/s 
-
-exit  
---
+++ PCB LOGO-画法集合-PCB工具-规则文件/SCH&PCB 设计、计算和检查的工具和软件/一键删除AD的History文件和Previews文件.bat
@@ -0,0 +1,9 @@
+del *.PcbDoc.Zip/s  
+del *.SchDoc.Zip/s 
+del *.PrjPCB.Zip/s 
+del *.PcbLib.Zip/s 
+del *.SchLib.Zip/s 
+del *.SchDocPreview/s 
+del *.HarnessPreview/s 
+
+exit  
